672c906b2452b095606f7d14b6fb9532ca30d26b
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2017-02-08 16:27:32 -0700

    5th update

--- client/app/components/bookmarks/bookmarks.js
+++ client/app/components/bookmarks/bookmarks.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import bookmarksComponent from './bookmarks.component';
+import SaveBookmarkModule from './save-bookmark/save-bookmark';
 
 const BookmarksModule = angular.module('bookmarks', [])
   .component('bookmarks', bookmarksComponent);
